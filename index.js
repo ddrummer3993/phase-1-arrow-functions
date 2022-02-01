89bc8ed00a0a858ac04ec565e571cf0c16616052
David Drummer <david.b.drummer@gmail.com>
2022-02-01 14:20:12 -0500

    commit tree 6e4ef63e78fcbeec14b71abe428bb179d16c5f27

---
+++ index.js
@@ -0,0 +1,14 @@
+// create function: divide
+
+const divide = function() {
+    return 2000 / 100;
+}
+
+//create fubctuion: square
+
+const square = x => x * x ;
+
+// create function: add
+
+const add = (x, y) => x + y ;
+
